70084f15383a6bb97a3e46a2c1b8d4d2d7e3bd5b
ShadmanSakibTalukder <rushansakib@gmail.com>
2023-09-09 00:02:32 +0600

    category error fixed

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -50,7 +50,7 @@ class CategoryController extends Controller
 
 public function edit(Category $category = null)
 {
-  return view('category.edit',compact('$category'));
+  return view('category.edit',compact('category'));
 }
 
 public function update(CategoryFormRequest $request, $category)
